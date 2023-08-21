fc8e0eb70e215bc1d242f965c11527be04d043c5
StefanKumarasinghe <89747418+StefanKumarasinghe@users.noreply.github.com>
2023-08-21 23:04:56 +1000

    Updated Explore

--- src/Explore.js
+++ src/Explore.js
@@ -6,11 +6,91 @@ import './App.css';
 
 function Explore() {
     const [assets, setAssets] = useState([]);
-    const [filteredAssets, setFilteredAssets] = useState([{"id":"1","symbol":"btc","desc":"Artwork by Stefan with a depiction of how sexy Stefan is....","name":"ArtWork Maniac","image":"https://th.bing.com/th/id/OIP.eupcce_RCFpqH2fsWKrLBAHaE7?pid=ImgDet&rs=1","current_price":3},{"id":"2","symbol":"btc","name":"Dania Maniac","desc":"Artwork by Stefan with a depiction of how sexy Stefan is....","image":"https://th.bing.com/th/id/OIP.p86bUO9ND1fxwX57R806PQHaHa?pid=ImgDet&w=900&h=900&rs=1","current_price":3}]);
+    const [filteredAssets, setFilteredAssets] = useState([
+        {
+          "id": "1",
+          "symbol": "btc",
+          "desc": "Artwork by Stefan with a depiction of how sexy Stefan is...",
+          "name": "ArtWork Maniac",
+          "image": "https://source.unsplash.com/random/art?1",
+          "current_price": 3
+        },
+        {
+          "id": "2",
+          "symbol": "btc",
+          "desc": "Artwork by Stefan with a depiction of how sexy Stefan is...",
+          "name": "Dania Maniac",
+          "image": "https://source.unsplash.com/random/art?2",
+          "current_price": 3
+        },
+        {
+          "id": "3",
+          "symbol": "btc",
+          "desc": "Abstract art showcasing the beauty of nature.",
+          "name": "Nature's Abstract",
+          "image": "https://source.unsplash.com/random/art?3",
+          "current_price": 4
+        },
+        {
+          "id": "4",
+          "symbol": "btc",
+          "desc": "Artwork focusing on geometric patterns.",
+          "name": "Geometric Magic",
+          "image": "https://source.unsplash.com/random/art?4",
+          "current_price": 5
+        },
+        {
+          "id": "5",
+          "symbol": "btc",
+          "desc": "Art inspired by the wonders of the universe.",
+          "name": "Galactic Dreams",
+          "image": "https://source.unsplash.com/random/art?5",
+          "current_price": 6
+        },
+        {
+          "id": "6",
+          "symbol": "btc",
+          "desc": "Art that mirrors the complexity of human emotions.",
+          "name": "Emotion Spectrum",
+          "image": "https://source.unsplash.com/random/art?6",
+          "current_price": 3.5
+        },
+        {
+          "id": "7",
+          "symbol": "btc",
+          "desc": "An artistic representation of time's relentless march.",
+          "name": "Eternal Clock",
+          "image": "https://source.unsplash.com/random/art?7",
+          "current_price": 7
+        },
+        {
+          "id": "8",
+          "symbol": "btc",
+          "desc": "A colorful depiction of summer's joy.",
+          "name": "Summer Palette",
+          "image": "https://source.unsplash.com/random/art?8",
+          "current_price": 4.5
+        },
+        {
+          "id": "9",
+          "symbol": "btc",
+          "desc": "The dance of colors on a canvas.",
+          "name": "Color Waltz",
+          "image": "https://source.unsplash.com/random/art?9",
+          "current_price": 5.5
+        },
+        {
+          "id": "10",
+          "symbol": "btc",
+          "desc": "The serenity of winter captured in art.",
+          "name": "Winter Calm",
+          "image": "https://source.unsplash.com/random/art?10",
+          "current_price": 4
+        }
+      ]
+      );
     const [search, setSearch] = useState();
-    const [selectedAsset, setSelectedAsset] = useState(null);
-    const blockcypherToken = '317b022b37cf41118924ca48d8627365';
-    const { cart, addToCart, removeFromCart } = useCart();
+    const { cart, addToCart} = useCart();
     useEffect(() => {
                 setAssets([{"id":"1","symbol":"btc","desc":"Artwork by Stefan with a depiction of how sexy Stefan is....","name":"ArtWork Maniac","image":"https://th.bing.com/th/id/OIP.eupcce_RCFpqH2fsWKrLBAHaE7?pid=ImgDet&rs=1","current_price":3},{"id":"2","symbol":"btc","name":"Dania Maniac","desc":"Artwork by Stefan with a depiction of how sexy Stefan is....","image":"https://th.bing.com/th/id/OIP.p86bUO9ND1fxwX57R806PQHaHa?pid=ImgDet&w=900&h=900&rs=1","current_price":3}]);
     }, []);
@@ -20,9 +100,9 @@ function Explore() {
         setSearch(value);
 
         if (value === '') {
-            setFilteredAssets(assets);
+            setFilteredAssets(filteredAssets);
         } else {
-            const filtered = assets.filter(asset => asset.name.toLowerCase().includes(value.toLowerCase()));
+            const filtered = filteredAssets.filter(asset => asset.name.toLowerCase().includes(value.toLowerCase()));
             setFilteredAssets(filtered);
         }
     };
@@ -46,7 +126,7 @@ function Explore() {
                                 
                                 <div class="w-100 row my-2">
                                     <div class="col-sm-7">
-                                    <a href="" class="btn btn-secondary">All</a> 
+                                    <a href="" class="btn btn-secondary px-4">All</a> 
                                     <a href="" class="btn text-white ">Music</a>        <a href="" class="btn text-white mx-1">Fantom</a>
                                     <a href="" class="btn text-white mx-1">Art</a>
                                     <a href="" class="btn text-white  mx-1">Token</a>
