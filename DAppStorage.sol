56809f848db8eeab76353488027ee98f193dad23
3293dhsin <joe5763481@gmail.com>
2023-10-08 11:44:50 +1100

    Smart Contract

---
+++ DAppStorage.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.6.0 <0.9.0;
+
+contract DAppStorage {
+    
+    struct Transaction {
+        uint256 transactionId;
+        address userAddress;
+        uint256 itemId;
+        uint256 totalPrice;
+    }
+
+    Transaction[] public transactions;
+    uint256 public transactionCount;
+
+    event TransactionAdded(uint256 indexed transactionId, address indexed userAddress, uint256 indexed itemId, uint256 totalPrice);
+
+    function addTransaction(address _userAddress, uint256 _itemId, uint256 _totalPrice) public {
+        uint256 newTransactionId = transactionCount++;
+        transactions.push(Transaction(newTransactionId, _userAddress, _itemId, _totalPrice));
+        emit TransactionAdded(newTransactionId, _userAddress, _itemId, _totalPrice);
+    }
+
+    function getTransactionsCount() public view returns (uint256) {
+        return transactionCount;
+    }
+
+    function getTransaction(uint256 _transactionId) public view returns (uint256, address, uint256, uint256) {
+        require(_transactionId < transactionCount, "Transaction does not exist");
+        Transaction memory transaction = transactions[_transactionId];
+        return (transaction.transactionId, transaction.userAddress, transaction.itemId, transaction.totalPrice);
+    }
+}
